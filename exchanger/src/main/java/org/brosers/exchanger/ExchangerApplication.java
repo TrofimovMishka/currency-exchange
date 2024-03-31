6ad84ea55ee8d6e7c9eb5d4a0e53f569528c378a
Dron <andrii.linkoln@gmail.com>
2024-03-31 21:02:53 +0200

    Add starter

---
+++ exchanger/src/main/java/org/brosers/exchanger/ExchangerApplication.java
@@ -0,0 +1,13 @@
+package org.brosers.exchanger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExchangerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExchangerApplication.class, args);
+	}
+
+}
